0b6899dde02f924ab9a32a74f067ae6a9ec3267c
Akhil Eldho <akhil.p.eldho@gmail.com>
2024-07-29 01:09:00 +1200

    Payment

--- BackEnd/Barely_Cooking/Barely_Cooking/Controllers/PaymentController.cs
+++ BackEnd/Barely_Cooking/Barely_Cooking/Controllers/PaymentController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Barely_Cooking_API.Data;
+using Barely_Cooking_API.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Stripe;
+using System.Net;
 
 namespace Barely_Cooking_API.Controllers
 {
@@ -7,5 +12,59 @@ namespace Barely_Cooking_API.Controllers
     [ApiController]
     public class PaymentController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+        protected ApiResponse _apiResponse;
+        public readonly IConfiguration _configuration;
+
+        public PaymentController(ApplicationDbContext context, IConfiguration iConfiguartion)
+        {
+            _context = context;
+            _configuration = iConfiguartion;
+            _apiResponse = new ApiResponse();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse>> MakePayment(string userId)
+        {
+            ShoppingCart cart = _context.ShoppingCarts.Include(x => x.Cart).ThenInclude(x => x.Cake).FirstOrDefault(x => x.UserId == userId);
+
+            if (cart == null || cart.Cart == null || cart.Cart.Count() == 0)
+            {
+                _apiResponse.StatusCode = HttpStatusCode.BadRequest;
+                _apiResponse.IsSuccess = false;
+
+                return BadRequest(_apiResponse);
+            }
+
+            #region Create Payment Intent
+
+            StripeConfiguration.ApiKey = _configuration["StripeSettings:SecretKey"];
+
+            cart.CartTotal = cart.Cart.Sum(x => x.Quantity * x.Cake.Price);
+
+            PaymentIntentCreateOptions payment = new()
+            {
+                Amount = (int)(cart.CartTotal * 100),
+                Currency = "nzd",
+                PaymentMethodTypes = new List<string>
+                {
+                    "card"
+                }
+            };
+
+            PaymentIntentService paymentService = new();
+            PaymentIntent paymentIntent = paymentService.Create(payment);
+
+            cart.StripePaymentIntedId = paymentIntent.Id;
+            cart.ClientSecret = paymentIntent.ClientSecret;
+
+            #endregion
+
+            _apiResponse.Result = cart;
+            _apiResponse.StatusCode = HttpStatusCode.OK;
+
+            return Ok(_apiResponse);
+        }
+
     }
 }
